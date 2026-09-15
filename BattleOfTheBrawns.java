86423d51058a754dac238725673327fe2cf2c4c1
Nikhil Bhalla _ Student - WakeIBTechEC <nbhalla@students.wcpss.net>
2026-09-15 13:18:49 -0400

    Main class initial code

---
+++ BattleOfTheBrawns.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+public class BattleOfTheBrawns {
+    public static void main(String[] args) {
+        // Initialize variables here
+        Scanner input = new Scanner(System.in);
+
+        System.out.println("Welcome to the Battle of the Brawns!");
+        System.out.println("What is your name?");
+        String name = input.nextLine();
+        
+    }
+}
