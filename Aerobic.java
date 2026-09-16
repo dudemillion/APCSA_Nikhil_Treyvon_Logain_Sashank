ed6aeac9470fa132f9c9944414632a12e9a0a003
Treyvon <149333746+TreyvoPrime@users.noreply.github.com>
2026-09-16 13:06:00 -0400

    Aerobic entry

---
+++ Aerobic.java
@@ -0,0 +1,14 @@
+public class Aerobic {
+    double Stamina;
+    double JumpPower;
+    public Aerobic(double Stamina, double JumpPower) {
+		this.Stamina = Stamina;
+		this.JumpPower=JumpPower;
+	}
+	public double GetStamina() {
+	    return this.Stamina;
+	}
+	public double GetJump(){
+	    return this.JumpPower;
+	}
+}
