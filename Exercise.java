352ec14cffcde029fcb3af1966f9ddaad2260b71
Sashank Kondraju <sashank.kvvs@gmail.com>
2026-09-16 12:59:18 -0400

    The code creates 4 methods and defines the variables

---
+++ Exercise.java
@@ -0,0 +1,28 @@
+
+
+public class Exercises
+{
+    private double time ;
+    private String type ;
+    private int xpgain ;
+    private String equipment;
+    
+    public Exercises(String newType, double newTime, int newxpgain,String newEquipment){
+        this.type = newType;
+        this.time = newTime;
+        this.xpgain = newxpgain;
+        this.equipment = newEquipment;
+    } 
+    public double getTime(){
+        return this.time;
+    }
+    public String getType(){
+        return this.type;
+    }
+    public String getEquipment(){
+        return this.equipment;
+    }
+    public int getXpgain(){
+        return this.xpgain;
+    }
+}
