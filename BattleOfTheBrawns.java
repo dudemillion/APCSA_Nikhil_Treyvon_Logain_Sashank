17d7dedba690dbc5a0b78d28f26e65cfd18c0d10
Nikhil Bhalla _ Student - WakeIBTechEC <nbhalla@students.wcpss.net>
2026-09-16 13:13:20 -0400

    edited exercises to make easily usable

--- BattleOfTheBrawns.java
+++ BattleOfTheBrawns.java
@@ -5,16 +5,16 @@ public class BattleOfTheBrawns {
         Scanner input = new Scanner(System.in);
         Exercise[] challenges = new Exercise[10];
         // Example challenges (use if you want)
-        challenges[0] = new Exercise("Push-ups", 10.0, 10, "Strength");
-        challenges[1] = new Exercise("Jumping Jacks", 5.0, 10, "Aerobics");
-        challenges[2] = new Exercise("Squats", 10.0, 10, "Strength");
-        challenges[3] = new Exercise("Burpees", 5.0, 10, "Aerobics");
-        challenges[4] = new Exercise("Lunges", 10.0, 10, "Strength");
-        challenges[5] = new Exercise("Mountain Climbers", 5.0, 10, "Aerobics");
-        challenges[6] = new Exercise("Plank", 5.0, 10, "Aerobics");
-        challenges[7] = new Exercise("High Knees", 5.0, 10, "Aerobics");
-        challenges[8] = new Exercise("Bicep Curls", 10.0, 10, "Strength");
-        challenges[9] = new Exercise("Jump Rope", 5.0, 10, "Aerobics");
+        challenges[0] = new Exercise("Push-ups", "Strength", 10.0, 10, false);
+        challenges[1] = new Exercise("Jumping Jacks", "Aerobics", 5.0, 10, false);
+        challenges[2] = new Exercise("Squats", "Strength", 10.0, 10, false);
+        challenges[3] = new Exercise("Burpees", "Aerobics", 5.0, 10, false);
+        challenges[4] = new Exercise("Lunges", "Strength", 10.0, 10, false);
+        challenges[5] = new Exercise("Mountain Climbers", "Aerobics", 5.0, 10, false);
+        challenges[6] = new Exercise("Plank", "Aerobics", 5.0, 10, false);
+        challenges[7] = new Exercise("High Knees", "Aerobics", 5.0, 10, false);
+        challenges[8] = new Exercise("Bicep Curls", "Strength", 10.0, 10, false);
+        challenges[9] = new Exercise("Jump Rope", "Aerobics", 5.0, 10, false);
 
         Challenger[] challengers = new Challenger[10];
         // Example challengers (use if you want)
@@ -42,5 +42,6 @@ public class BattleOfTheBrawns {
         System.out.println("These are your available challenges:");
 
         // Add code to display challenges based on user input
+
     }
 }
--- Exercise.java
+++ Exercise.java
@@ -1,23 +1,28 @@
 public class Exercise {
-    
+
+    private String name ;
     private double time ;
     private String type ;
     private int xpgain ;
-    private String equipment;
+    private boolean equipment;
     
-    public Exercise(String newType, double newTime, int newxpgain,String newEquipment){
+    public Exercise(String newName, String newType, double newTime, int newxpgain, boolean newEquipment){
+        this.name = newName;
         this.type = newType;
         this.time = newTime;
         this.xpgain = newxpgain;
         this.equipment = newEquipment;
     } 
-    public double getTime(){
-        return this.time;
+    public String getName(){
+        return this.name;
     }
     public String getType(){
         return this.type;
     }
-    public String getEquipment(){
+    public double getTime(){
+        return this.time;
+    }
+    public boolean getEquipment(){
         return this.equipment;
     }
     public int getXpgain(){
